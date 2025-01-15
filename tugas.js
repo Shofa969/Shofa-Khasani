57efbd365dc24dc9e8fa3fe24f850d71a497422a
Shofa969 <oicuk4400@gmail.com>
2025-01-15 11:16:03 +0700

    Add files via upload

---
+++ tugas.js
@@ -0,0 +1,17 @@
+let namaDepan = "shofa";
+let namaBelakang = "khasani";
+let alamat = "sayung";
+let usia = "16";
+let ttl = "Demak,24 Februari 2008";
+let gender = "Laki-laki";
+let agama = "Islam";
+let email = "oicuk004@gmail.com";
+let berat = "55";
+let tinggi = "161";
+let hobi = "mancing";
+let cita = "mekanik";
+let pekerjaan = "pelajar";
+let sekolah = "smkn";
+let jurusan = "PPLG";
+
+document.write("nama saya adalah" + namaDepan + " " + namaBelakang + "saya tingggal di" + alamat + "pada tahun 2024 ini usia saya adalah" + usia + "saya lahir di" + ttl + "jenis kelamin saya adalah" + gender + "saya beragama" + agama + "alamat email saya adalah" + email + "saya memiliki berat badan" + berat + "kg dengan tinggi" + tinggi + "cm" + "saya sangat senang" + hobi + "karena saya memiliki cita-cita" + cita + "saat ini saya adalah seorang" + pekerjaan + "yang bersekolah di" + sekolah + "pada jurusan" + jurusan);
